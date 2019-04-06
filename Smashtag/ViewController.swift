6c6587c213da44e0081d903c711ad3535b6dcfbc
Peter Forward <pgforward@gmail.com>
2019-04-05 18:06:00 -0700

    Initial Commit

---
+++ Smashtag/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Smashtag
+//
+//  Created by Peter Forward on 4/5/19.
+//  Copyright © 2019 Peter Forward. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
